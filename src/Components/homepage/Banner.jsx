f77405a046c9129c48070de9add2191339e8fa80
parvez-nur-shadhin <parveznur00@gmail.com>
2026-05-04 01:51:14 +0600

    Animate.css npm package used!

--- src/Components/homepage/Banner.jsx
+++ src/Components/homepage/Banner.jsx
@@ -1,5 +1,6 @@
 import summerSalePhoto from "@/assets/summer-sale.png";
 import Image from "next/image";
+import 'animate.css';
 
 const Banner = () => {
   return (
@@ -7,9 +8,9 @@ const Banner = () => {
       className={`container mx-auto p-10 bg-cover bg-center text-center lg:py-30 space-y-5 rounded-2xl mt-10`}
       style={{ backgroundImage: `url(${summerSalePhoto.src})` }}
     >
-      <h1 className="text-white text-6xl font-bold">Summer Sale!</h1>
-      <p className="text-4xl text-blue-300 font-semibold">Hot Deals</p>
-      <p className="text-2xl text-blue-200">Up to 50% Off</p>
+      <h1 className="text-white text-6xl font-bold animate__animated animate__backInDown">Summer Sale!</h1>
+      <p className="text-4xl text-blue-300 font-semibold animate__animated animate__backInDown">Hot Deals</p>
+      <p className="text-2xl text-blue-200 animate__animated animate__backInDown">Up to 50% Off</p>
     </div>
   );
 };
