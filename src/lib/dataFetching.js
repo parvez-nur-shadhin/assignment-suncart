995151c05e496c3a7c2ea7a479b7e172c74bfe94
parvez-nur-shadhin <parveznur00@gmail.com>
2026-05-03 22:27:32 +0600

    Local Link changed

--- src/lib/dataFetching.js
+++ src/lib/dataFetching.js
@@ -1,11 +1,11 @@
 export const getProducts= async() => {
-    const res = await fetch('http://localhost:3000/data.json', {cache: "no-store"});
+    const res = await fetch('http://assignment-suncart.vercel.app/data.json', {cache: "no-store"});
     const data = await res.json();
     return data;
 }
 
 export const getSummerCareTips = async() => {
-    const res = await fetch('http://localhost:3000/summerCareTips.json', {cache: "no-store"});
+    const res = await fetch('http://assignment-suncart.vercel.app/summerCareTips.json', {cache: "no-store"});
     const data = await res.json();
     return data;
 }
