19e13966c7ff41be88e6e82e9386c2fd552f63ac
parvez-nur-shadhin <parveznur00@gmail.com>
2026-05-03 22:46:39 +0600

    Cache removed

--- src/lib/dataFetching.js
+++ src/lib/dataFetching.js
@@ -1,11 +1,11 @@
 export const getProducts= async() => {
-    const res = await fetch('http://assignment-suncart.vercel.app/data.json', {cache: "no-store"});
+    const res = await fetch('http://assignment-suncart.vercel.app/data.json');
     const data = await res.json();
     return data;
 }
 
 export const getSummerCareTips = async() => {
-    const res = await fetch('http://assignment-suncart.vercel.app/summerCareTips.json', {cache: "no-store"});
+    const res = await fetch('http://assignment-suncart.vercel.app/summerCareTips.json');
     const data = await res.json();
     return data;
 }
