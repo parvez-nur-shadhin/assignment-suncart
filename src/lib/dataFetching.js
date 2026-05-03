d2224fffc1b217cb076e3c7c6f4d1ade25fddf18
parvez-nur-shadhin <parveznur00@gmail.com>
2026-05-03 22:17:52 +0600

    Cache added

--- src/lib/dataFetching.js
+++ src/lib/dataFetching.js
@@ -1,11 +1,11 @@
 export const getProducts= async() => {
-    const res = await fetch('http://localhost:3000/data.json');
+    const res = await fetch('http://localhost:3000/data.json', {cache: "no-store"});
     const data = await res.json();
     return data;
 }
 
 export const getSummerCareTips = async() => {
-    const res = await fetch('http://localhost:3000/summerCareTips.json');
+    const res = await fetch('http://localhost:3000/summerCareTips.json', {cache: "no-store"});
     const data = await res.json();
     return data;
 }
